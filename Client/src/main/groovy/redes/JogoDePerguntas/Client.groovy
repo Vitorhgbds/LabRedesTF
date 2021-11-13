cfb2fd19257b2a8452aac0950efe0b53e2531fb9
jp martins <joao@ubots.com.br>
2021-11-13 15:54:01 -0300

    removendo o lonbok, preciso disso naum...

--- Client/src/main/groovy/redes/JogoDePerguntas/Client.groovy
+++ Client/src/main/groovy/redes/JogoDePerguntas/Client.groovy
@@ -3,20 +3,7 @@ package redes.JogoDePerguntas
 import redes.JogoDePerguntas.UDP.UDPClient
 
 class Client {
-
-    @Lazy
-    private static Properties properties
-
     public static void main(String[] args) {
-        new Object() {}
-                .getClass()
-                .getResource( "/application.properties" )
-                .withInputStream {
-                    properties.load(it)
-                }
         UDPClient.Client()
-
     }
-
-
 }
--- Client/src/main/groovy/redes/JogoDePerguntas/UDP/UDPClient.groovy
+++ Client/src/main/groovy/redes/JogoDePerguntas/UDP/UDPClient.groovy
@@ -5,34 +5,35 @@ class UDPClient {
     def static socket
     def static data
     def static i
-    def static Client(){
+
+    def static Client() {
         // Para verificar se a porta ta ocupada, se ela estiver tenta na porta seguinte
-        boolean aux=true
-        while (aux){
-            aux=false
-            try{
-                socket =new DatagramSocket(port);
-            }catch(ignored){
+        boolean aux = true
+        while (aux) {
+            aux = false
+            try {
+                socket = new DatagramSocket(port)
+            } catch (ignored) {
                 aux = true
                 port++
             }
         }
         data = "First Contact".getBytes("ASCII")
-        i =0;
+        i = 0;
         new Thread(socketWatch).start()
-        while (true){
-            if(i!=0) {
+        while (true) {
+            if (i != 0) {
                 data = System.in.newReader().readLine()
                 data = data.getBytes("ASCII")
             }
-            if (data=="-1") {
-                try{
+            if (data == "-1") {
+                try {
                     System.exit(0)
-                }catch(ignored){
+                } catch (ignored) {
 
                 }
             }
-            def packet = new DatagramPacket(data, data.length,InetAddress.getByName("localhost"),5000)
+            def packet = new DatagramPacket(data, data.length, InetAddress.getByName("localhost"), 5000)
             socket.send(packet)
             i++
         }
@@ -45,19 +46,19 @@ class UDPClient {
     static Runnable socketWatch = new Runnable() {
         @Override
         void run() {
-            while (true){
-                try{
-                    def buffer = (' ' * 4096) as byte[]
-                    def response = new DatagramPacket(buffer, buffer.length)
+            while (true) {
+                try {
+                    byte[] buffer = (' ' * 4096) as byte[]
+                    DatagramPacket response = new DatagramPacket(buffer, buffer.length)
                     socket.receive(response)
-                    def s = new String(response.data, 0, response.length)
+                    String s = new String(response.data, 0, response.length)
                     println s
                     // - area que avisa ao servidor após receber resposta, para testar o reenvio de mensagens do servidor comentar essa parte:
                     data = "Received".getBytes("ASCII")
-                    def packet = new DatagramPacket(data, data.length,InetAddress.getByName("localhost"),5000)
+                    DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName("localhost"), 5000)
                     socket.send(packet)
 
-                }catch(ignored){}
+                } catch (ignored) {}
             }
         }
     }
