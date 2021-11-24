332e46c7f312b2fc2fb7e36e75d6fe83c6908eda
jp martins <joao@ubots.com.br>
2021-11-23 23:30:37 -0300

    messageSender handler + removendo game

--- Server/src/main/groovy/redes/JogoDePerguntas/Server.groovy
+++ Server/src/main/groovy/redes/JogoDePerguntas/Server.groovy
@@ -4,19 +4,7 @@ import redes.JogoDePerguntas.UDP.UDPServer
 
 class Server {
 
-    @Lazy
-    private static Properties properties
-
-    public static void main(String[] args) {
-        new Object() {}
-                .getClass()
-                .getResource( "/application.properties" )
-                .withInputStream {
-                    properties.load(it)
-                }
-        UDPServer.Server()
-
+    static void main(String[] args) {
+        UDPServer.server()
     }
-
-
 }
--- Server/src/main/groovy/redes/JogoDePerguntas/UDP/UDPServer.groovy
+++ Server/src/main/groovy/redes/JogoDePerguntas/UDP/UDPServer.groovy
@@ -1,45 +1,42 @@
 package redes.JogoDePerguntas.UDP
+
 import redes.JogoDePerguntas.Server
-import redes.JogoDePerguntas.Game.GameMode
-import redes.JogoDePerguntas.Game.Game
 import redes.JogoDePerguntas.Game.Player
 
 class UDPServer {
     static Map<Integer, Player> playerList
-    static volatile Map<Integer,Integer> waitingResponse
-    static volatile Map<Integer,Integer> waitingResponseCount
+    static volatile Map<Integer, Integer> waitingResponse
+    static volatile Map<Integer, Integer> waitingResponseCount
     static volatile int count
-    static Map<Integer,String> lastResponses
-    def static socketServer
+    static Map<Integer, String> lastResponses
+    static DatagramSocket socketServer
 
     /*
         Servidor
     */
-    def static Server(){
+
+    static void server() {
         socketServer = new DatagramSocket(5000)
         lastResponses = new HashMap<>()
         // aqui ele reenvia a mensagem (ou sabe quando reenviar)
         waitingResponse = new HashMap<>()
         // aqui ele ta contando quantas mensagens já reenviou
         waitingResponseCount = new HashMap<>()
-        def buffer = (' ' * 4096) as byte[]
+        byte[] buffer = (' ' * 4096) as byte[]
         new Thread(watch).start()
-        
+
         // da pra remover
         playerList = new HashMap<>()
-        Game game = new Game()
-        game.Menu()
-        
-        
-        while(true) {
-            def incoming = new DatagramPacket(buffer, buffer.length)
+
+
+        while (true) {
+            DatagramPacket incoming = new DatagramPacket(buffer, buffer.length)
             socketServer.receive(incoming)
-            def s = new String(incoming.data, 0, incoming.length)
-            String reply = Response(s, game, incoming.port)
-            if(!reply.equals("Received"))
-            {
-                lastResponses.put(incoming.port,reply)
-                def outgoing = new DatagramPacket(reply.bytes, reply.size(),
+            String clientMessage = new String(incoming.data, 0, incoming.length)
+            String reply = handleResponse(clientMessage, incoming.port)
+            if (reply != "Received") {
+                lastResponses.put(incoming.port, reply)
+                DatagramPacket outgoing = new DatagramPacket(reply.bytes, reply.size(),
                         incoming.address, incoming.port);
                 socketServer.send(outgoing)
             }
@@ -48,17 +45,17 @@ class UDPServer {
     /*
         Thread para verificar se o cliente nao respondeu
     */
-     static Runnable watch = new Runnable() {
+    static Runnable watch = new Runnable() {
 
         @Override
         void run() {
-            while (true){
-                try{
+            while (true) {
+                try {
                     Thread.sleep(1000)
-                    waitingResponse.each {clientPort,waitingCountTime ->
-                        if(waitingCountTime<1) {
+                    waitingResponse.each { clientPort, waitingCountTime ->
+                        if (waitingCountTime < 1) {
                             count = waitingResponseCount.get(clientPort)
-                            if (count<=2) {
+                            if (count <= 2) {
                                 count++
                                 waitingResponseCount.put(clientPort, count)
                                 sendResponseAgain(clientPort)
@@ -70,11 +67,12 @@ class UDPServer {
                             }
                         } else {
                             waitingCountTime--
-                            println(clientPort + " "+ waitingCountTime)
-                            waitingResponse.put(clientPort,waitingCountTime)
+                            println(clientPort + " " + waitingCountTime)
+                            waitingResponse.put(clientPort, waitingCountTime)
                         }
                     }
-                }catch(ignored){}
+                } catch (ignored) {
+                }
             }
         }
     }
@@ -82,10 +80,11 @@ class UDPServer {
     /*
         Envia a ultima mensagem do servidor novamente
     */
-    static def sendResponseAgain(int port){
+
+    static def sendResponseAgain(int port) {
         String reply = lastResponses.get(port)
         DatagramPacket outgoing = new DatagramPacket(reply.bytes, reply.size(),
-                InetAddress.getByName("localhost") , port)
+                InetAddress.getByName("localhost"), port)
         socketServer.send(outgoing)
     }
 
@@ -93,57 +92,8 @@ class UDPServer {
     /*
         Gerencia as respostas recebidas pelo cliente
     */
-    def static Response(String s, Game game, Integer port){
-        println(port)
-        waitingResponse.put(port, Server.properties."response.time" as int) // Precisa dessa complexidade toda naum. da pra fazer no mapa mesmo. depois eu ajusto se for o caso :Jp
-        waitingResponseCount.put(port, 0)
-        switch (s){
 
-        /*
-            Mensagem de resposta do cliente
-        */
-            case "Received": println(port + " Recebeu a mensagem")
-                waitingResponse.remove(port)
-                return "Received"
-                break;
-            case "First Contact":
-                playerList.put(port,new Player())
-                return Game.menu;
-                break;
-        //---------------- da pra remover ----------------//
-        /*
-            Mensagem que retorna o menu
-        */
-            case "0": return game.Menu()
-                break
-        /*
-            Mensagens que definem o modo de jogo
-        */
-            case "1": playerList.get(port).mode = GameMode.FACIL
-                game.Start(playerList.get(port))
-                return "Jogo facil iniciado \n\n" + game.getQuestion(playerList.get(port))
-                break;
-            case "2":
-                playerList.get(port).mode = GameMode.MEDIO
-                game.Start(playerList.get(port))
-                return "Jogo medio iniciado \n\n" + game.getQuestion(playerList.get(port))
-                break
-            case "3": playerList.get(port).mode = GameMode.DIFICIL
-                game.Start(playerList.get(port))
-                return "Jogo dificil  iniciado \n\n" + game.getQuestion(playerList.get(port))
-                break
-        /*
-            Mensagens que mostram as alternativas ecolhidas
-        */
-            case "A": return game.VerifyAnswer("A",playerList.get(port)) +"\n\n" + game.getQuestion(playerList.get(port))
-                break
-            case "B": return game.VerifyAnswer("B",playerList.get(port)) +"\n\n" + game.getQuestion(playerList.get(port))
-                break
-            case "C": return game.VerifyAnswer("C",playerList.get(port)) +"\n\n" + game.getQuestion(playerList.get(port))
-                break
-            case "D": return game.VerifyAnswer("D",playerList.get(port)) +"\n\n" + game.getQuestion(playerList.get(port))
-                break
-            default: return "A resposta é invalida. Tente novamente"
-        }
+    static String handleResponse(String response, Integer port) {
+        return "OK"
     }
 }
